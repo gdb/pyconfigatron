87759fb86298c1086e2f84a7ced5f3844113ee9c
Greg Brockman <gdb@gregbrockman.com>
2016-07-24 19:32:09 -0700

    Actually include packages

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys, os.path
 
 setup(name='pyconfigatron',
-      version='0.0.1',
+      version='0.0.2',
+      packages=[package for package in find_packages()
+                if package.startswith('pyconfigatron')],
 )
